2b95542d9c2f2ebf9b6845f1db1040f117333721
mateusdev3400 <mateusdev3400@gmail.com>
2023-12-18 16:47:16 -0300

    feat: variables and list of available tickets

---
+++ js/mock/tickets.js
@@ -0,0 +1,5 @@
+export const tickets = [
+    { idTicket: 1, titleTicket: "Pista", amountTicket: 100, statusTicket: true },
+    { idTicket: 2, titleTicket: "Cadeira Superior", amountTicket: 200, statusTicket: true },
+    { idTicket: 3, titleTicket: "Cadeira Inferior", amountTicket: 200, statusTicket: true }
+];
---
+++ js/variables/variables.js
@@ -0,0 +1,7 @@
+export let inputPlace = document.querySelector("#place");
+export let inputAmount = document.querySelector("#amount");
+
+export let buttonTicket = document.querySelector("#button-ticket");
+
+export let listMenuTicket = document.querySelector("#list-tickets");
+export let divMensageErro = document.querySelector("#mensage-erro");
